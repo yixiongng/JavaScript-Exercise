7443ff08fcfbeb10baade888fad37760f168fc08
yixiongng <45516541+yixiongng@users.noreply.github.com>
2019-03-06 22:34:15 +0800

    Update recursion.js

--- Chapter 3/recursion.js
+++ Chapter 3/recursion.js
@@ -1,19 +1,17 @@
-//recursion
-function isEven(num)
-{
-    if(num == 0)
+function isEven(num) {
+  if(num == 0) {
     return true;
-
-    else if(num == 1)
+  }
+  else if(num == 1) {
     return false;
-
-    else if(num<0)
+  }
+  else if(num < 0) {
     return isEven(-num);
-
-    else
+  }
+  else {
     return isEven(num-2);
+  } 
 }
-
 console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1));
