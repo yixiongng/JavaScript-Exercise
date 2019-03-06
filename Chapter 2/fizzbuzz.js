752d50925362d684b60d16c742167c1d78ba2fb5
yixiongng <45516541+yixiongng@users.noreply.github.com>
2019-03-06 22:25:21 +0800

    Update fizzbuzz.js

--- Chapter 2/fizzbuzz.js
+++ Chapter 2/fizzbuzz.js
@@ -1,30 +1,29 @@
-//fizzbuzz
 let div3 = "Fizz";
 let div5 = "Buzz";
-for(let i=1;i<=100;i++)
-{
-	if(i%3 == 0)
+for(let i = 1; i <= 100; i++) {
+	if(i % 3 == 0) {
 		console.log(div3);
-
-	else if(i%3 != 0 && i%5 == 0)
-	console.log(div5);
-
-	else
-	console.log(i);
+	}
+	else if(i % 3 != 0 && i % 5 == 0) {
+		console.log(div5);
+	}
+	else {
+    console.log(div5);
+  }
 }
 
 //fizzbuzz version 2
-for(let i=1;i<=100;i++)
-{
-	if(i%3 == 0 && i%5 !=0)
-		console.log(div3);
-
-	else if(i%3 != 0 && i%5 == 0)
-	console.log(div5);
-
-	else if(i%3 == 0 && i%5 == 0)
-	console.log(div3+div5);
-
-	else
-	console.log(i);
+for(let i = 1; i <= 100; i++) {
+	if(i % 3 == 0 && i % 5 != 0) {
+    console.log(div3);
+  }
+	else if(i % 3 != 0 && i % 5 == 0) {
+    console.log(div5);
+  }
+	else if(i % 3 == 0 && i % 5 == 0) {
+    console.log(div3 + div5);
+  }
+	else {
+    console.log(i);
+  }
 }
