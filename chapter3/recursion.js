3331980d865ed712ec5799b7f8844259af93a648
Wingless Bear <yixiongng@gmail.com>
2019-03-20 01:19:12 +0800

    recursion: Push exercise

---
+++ chapter3/recursion.js
@@ -0,0 +1,17 @@
+function isEven(num) {
+  if(num == 0) {
+    return true;
+  }
+  else if(num == 1) {
+    return false;
+  }
+  else if(num < 0) {
+    return isEven(-num);
+  }
+  else {
+    return isEven(num-2);
+  } 
+}
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
